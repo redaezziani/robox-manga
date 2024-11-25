3d1d363106d7d9091102cf95445a392310aa30c2
user <user@user.user>
2024-11-25 15:12:03 +0100

    --

---
+++ src/app/(main)/subscribers/categories/page.tsx
@@ -0,0 +1,14 @@
+import MainPageLayout from '@/components/shared-ui/layouts/main-page-layout'
+import React from 'react'
+
+const page = () => {
+  return (
+    <MainPageLayout>
+        <p>
+            hdfskdhikfghjdslkgj
+        </p>
+    </MainPageLayout>
+  )
+}
+
+export default page
---
+++ src/app/(main)/subscribers/logic/test.ts
@@ -0,0 +1,3 @@
+export const returnHello =()=>{
+    return "hello world"
+}
