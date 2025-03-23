2f15d7e99429bf020d953a2115fe81d40dbd04fc
reda ezziani <redaezziani.dev@gmail.com>
2025-03-23 21:37:35 +0000

    feat: enhance Next.js configuration with image remote patterns and build optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     images: {
-        domains: ['res.cloudinary.com'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          pathname: '/dk4wxo8wc/**',
+        },
+      ],
+    },
+    output: 'standalone',
+    eslint: {
+      // Disable ESLint during build
+      ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+      config.ignoreWarnings = [
+        { module: /node_modules/, message: /Critical dependency/ },
+      ];
+      return config;
     },
     async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://www.redaezziani.com/api/:path*',
-            },
-        ];
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://www.redaezziani.com/api/:path*',
+        },
+      ];
     },
-}
-
-module.exports = nextConfig
+  }
+  
+  module.exports = nextConfig
+  
